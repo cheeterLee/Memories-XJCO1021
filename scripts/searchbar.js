18ed68e3614622bb9890e08fded0db6e52e5edd4
cheeter_Lee <johnson991210@gmail.com>
2022-11-19 16:09:11 +0800

    add explore.html and searchbar.js

---
+++ scripts/searchbar.js
@@ -0,0 +1,29 @@
+// $(document).ready(function() {
+//     $('#search-bar').on('keyup', function() {
+//         let value = $(this).val().trim().toLowerCase()
+//         $('#card-container .card').filter(function() {
+//             $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+//         })
+//     })
+// })
+
+const cards = [...document.querySelectorAll('.card')]
+const searchBar = document.getElementById('search-bar')
+
+console.log(cards)
+
+searchBar.addEventListener('keyup', () => {
+    let input = searchBar.value.trim().toLowerCase()
+    console.log(input)
+    cards.forEach((card) => {
+        let title = card.querySelector('.card-title')
+        let text = card.querySelector('.card-text')
+        if (title.innerText.toLowerCase().indexOf(input) > -1 || text.innerText.toLowerCase().indexOf(input) > -1) {
+            card.style.display = ''
+        } else {
+            card.style.display = 'none'
+        }
+    })
+})
+
+
